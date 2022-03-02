9f044383af546e3fa311ab2b93253cb95f279614
atul rananavare <rananavareatul07@gmail.com>
2022-03-02 22:06:53 +0530

    updated

---
+++ Java8Practise/src/com/programs/Employee.java
@@ -0,0 +1,31 @@
+package com.programs;
+
+public class Employee {
+	
+	private int id;
+	private long salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "\n [id=" + id + ", salary=" + salary + "]";
+	}
+	public Employee(int id, long salary) {
+		super();
+		this.id = id;
+		this.salary = salary;
+	}
+	
+	
+
+}
---
+++ Java8Practise/src/com/programs/Java8EmpDemo.java
@@ -0,0 +1,24 @@
+package com.programs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Java8EmpDemo {
+
+	public static void main(String[] args) {
+
+		List<Employee> empList = new ArrayList<Employee>();
+
+		empList.add(new Employee(1, 500));
+		empList.add(new Employee(2, 1000));
+		empList.add(new Employee(3, 1500));
+		empList.add(new Employee(4, 350));
+		empList.add(new Employee(5, 4000));
+		empList.add(new Employee(6, 7000));
+
+		// sort employee based on theire salaries in desc order
+		List<Employee> e=empList.stream().sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary())).skip(3).collect(Collectors.toList());
+		System.out.println(e);
+	}
+}
